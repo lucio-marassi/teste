10b80dfa29a4d3311d6c856bf15f3c4bf67626ea
Lucio Marassi <luciomarassi@gmail.com>
2024-12-21 00:11:41 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Lucio Marassi é Demais!")
+
+#Variáveis
+idade = 49
+nome = "Lucio"
+
+print("Seu nome é",nome,"e sua idade é de",idade,"anos")
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
